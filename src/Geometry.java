752a8dd4fbc4f2993fe3fdf12fa36f85a57ec34d
marclmao <marcuslmao1@gmail.com>
2023-10-03 21:23:49 -0400

    FIXEDDDDDDDDDDDDD

--- src/Geometry.java
+++ src/Geometry.java
@@ -12,11 +12,11 @@ public class Geometry
         double integer_3 = scan.nextDouble();
         System.out.print("Enter y2 double ");
         double integer_4 = scan.nextDouble();
-        System.out.println(("The slope is  " + (integer_2 - integer_1) / (integer_4 - integer_3)));
-        System.out.print("The midpoint is " + (integer_1 + integer_1)/2.0);
-        System.out.println(" " + (integer_3 + integer_3)/2);
-        double length_before_sqrt_x = Math.pow((integer_1 - integer_2), 2);
-        double length_before_sqrt_y = Math.pow((integer_3 - integer_4), 2);
+        System.out.println(("The slope is  " + ((integer_4 - integer_3) / (integer_2 - integer_1))));
+        System.out.print("The midpoint is " + (integer_1 + integer_2)/2.0);
+        System.out.println(" " + (integer_3 + integer_4)/2.0);
+        double length_before_sqrt_x = Math.pow((integer_1 - integer_2), 2.0);
+        double length_before_sqrt_y = Math.pow((integer_3 - integer_4), 2.0);
         double length_before_sqrt = (length_before_sqrt_x + length_before_sqrt_y);
         double length = Math.sqrt(length_before_sqrt);
         System.out.println("Your length is " + length);
